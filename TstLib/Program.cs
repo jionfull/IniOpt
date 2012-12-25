e2167d2a09af93b073a45299ef1cfd1f24fb7357
张景福 <jionfull@163.com>
2012-12-25 12:58:52 +0800

    初次备份

---
+++ TstLib/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lon.IO;
+
+namespace TstLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IniFile a = new IniFile("abc.ini");
+            a.Load();
+            foreach(string str in a.GetAllSectionName() )
+            {
+                Console.WriteLine(str);
+                foreach(string rec in a.GetSectionItemName(str))
+                {
+                    Console.WriteLine(rec);
+                }
+            }
+            a.WriteString("ghi", "3", "4");
+            a.Save();
+            IniFile b = new IniFile("b.ini");
+            b.WriteString("g", "3", "4");
+            
+            b.Save();
+          
+          
+            Console.ReadKey();
+        }
+    }
+}
